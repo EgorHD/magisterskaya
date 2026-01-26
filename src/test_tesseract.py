c0c5223f7d8ca9211cae5dae0c44c903fd887771
EgorHD <egorkopanew@mail.ru>
2026-01-26 18:31:32 +0300

    delete_files

--- src/test_tesseract.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-from core.io.loader import load_document
-from core.ocr.factory import get_ocr_engine
-
-doc = load_document(Path("C:\Users\egork\OneDrive\Рабочий стол\тестик.pdf"), pdf_dpi=300)
-engine = get_ocr_engine()
-res = engine.recognize_document(doc)
-print(res.text()[:2000])
